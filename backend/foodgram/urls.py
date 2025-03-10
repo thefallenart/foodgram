24c947664411aae15ab383f2805cf8bb79275452
Kirill Salnikov <kirill18191@yandex.ru>
2025-03-10 03:09:20 +0300

    updated with main urls

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from api.views import short_link_redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path(
+        's/<str:short_code>/',
+        short_link_redirect,
+        name='short_link_redirect'
+    ),
 ]
